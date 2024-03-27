3af53a57402e98ebf2c220d8f6b096f902ae3159
Krzysztof174 <174903@student.uwm.edu.pl>
2024-03-27 17:04:12 +0100

    1

--- main.py
+++ main.py
@@ -1,57 +1,42 @@
 import sys
 import random
 import math
-
+import numpy as np
 def zad1():
-    print( round(( math.e**4 - math.log2(34) )**(1/3), 2))
-    print( round(( math.log(20) + math.cos(45) + math.e )**(1/3), 2))
-    print( round(( math.log(20, 3) + math.sin(45) * 5/8 )**(1/4), 2))
-    print( round(math.log(23, 3) + (math.sin(45) +5)**(1/3), 2) )
-    print(round( ( math.log(32, 2) + math.pi + math.sin(56) )**(1/4), 2))
+    a = np.array([3*i for i in range(1, 16)])
+    print(a)
 
 def zad2():
-    wysokosc = 0
-    try:
-        wysokosc = int(input("podaj wysokosc wiezy"))
-    except:
-        print("trzeba podac liczbe")
-        wysokosc = 0
-    if(wysokosc == 0):
-        return
-    if(wysokosc > 10):
-        return print("zbyt duza wysokosc") 
-    for i in range(1, wysokosc +1):
-        print("A"*i)
+    a = []
+    for i in range(1, 10):
+        a.append(i/10)
+    b = np.fromiter(a, dtype='int64')
+    print(b)
 
-def zad3():
-    wysokosc = 0
-    try:
-        wysokosc = int(input("podaj wysokosc wiezy"))
-    except:
-        print("trzeba podac liczbe")
-        wysokosc = 0
-    if(wysokosc == 0):
-        return
-    if(wysokosc > 10):
-        return print("zbyt duza wysokosc")
-    for i in range(1, wysokosc + 1):
-        print(round(wysokosc - i) * " " + "A"*i + "A" * (i-1))
+def zad3(n):
+    a = np.arange(1, 101)
+    b = a.reshape(10, 10)
+    return b
+
+def zad4(x, n):
+    a = np.logspace(1,n, num =n, base=x)
+    print(a)
 
 def zad5(n):
-    wektor = []
-    for i in range(0, n):
-        wektor.append([])
-        for z in range(0, n):
-            wektor[i].append(int(round(10*random.random(), 0)))
-    print(wektor)
-    for i in range(0, n):
-        suma = 0
-        for z in range(0, n):
-            suma += wektor[i][z]
-        print(suma)
+    a = np.arange(n,0, -1)
+    print(a)
+    b = np.diagonal([a, a])
+    print(b)
+
+def zad6():
+    a = np.array([['a', 'l', 'a'], ['', 'ó', ''], []], dtype=bytes)
+    print(a)
+
 if __name__ == '__main__':
     zad1()
     zad2()
-    zad3()
-    zad5(6)
+    print(zad3(10))
+    zad4(2, 4)
+    zad5(5)
+    zad6()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
