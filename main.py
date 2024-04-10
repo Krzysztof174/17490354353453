9ef6799fcdf829162276fd8c0e67c0e0ecf6d6b7
Krzysztof174 <174903@student.uwm.edu.pl>
2024-04-10 16:43:32 +0200

    numpy

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def zad2():
 def zad3(n):
     a = np.arange(1, 101)
     b = a.reshape(10, 10)
+    aa = np.zeros((n*n), dtype='int32')
     return b
 
 def zad4(x, n):
@@ -23,20 +24,57 @@ def zad4(x, n):
     print(a)
 
 def zad5(n):
-    a = np.arange(n,0, -1)
-    print(a)
-    b = np.diagonal([a, a])
-    print(b)
+    a = np.arange(n, 0, -1)
+    diag = np.diag(a)
+    return diag
 
 def zad6():
-    a = np.array([['a', 'l', 'a'], ['', 'ó', ''], []], dtype=bytes)
-    print(a)
+    mrowka = np.array(list('mrowka'))
+    wykreslanka = np.diag(mrowka)
+    wykreslanka[:, 0] = mrowka
+    for a in range(5):
+        wykreslanka[a, a] = mrowka[a]
+    print(wykreslanka)
+
+def zad7(n):
+    mac = np.zeros((n, n), dtype='int32')
+    mac += np.diag([2 for _ in range(n)])
+    for i in range(1, n):
+        mac += np.diag([2 + (2 * i) for _ in range(n - i)], k=i)
+        mac += np.diag([2 + (2 * i) for _ in range(n - i)], k=-i)
+
+def zad8(tab, kierunek = 'poziomo'):
+    if kierunek == 'poziomo':
+        if tab.shape[0]%2 != 0:
+            return
+        p1 = tab[0:int(tab.shape[0]/2), :]
+        p2 = tab[int(tab.shape[0] / 2), :]
+        print(p1)
+        print(p2)
+    elif kierunek == 'pionowo':
+        if tab.shape[1]%2 != 0:
+            p1 = tab[0:int(tab.shape[0]/2), :]
+            p2 = tab[int(tab.shape[0] / 2), :]
+    print(p1)
+    print(p2)
+
+def zad9(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return zad9(n-1) + zad9(n-2)
+
+def zad10():
+    a = np.arange(12).reshape((3, 4))
+    print(a.sum())
 
 if __name__ == '__main__':
     zad1()
     zad2()
     print(zad3(10))
     zad4(2, 4)
-    zad5(5)
+    print(zad5(5))
     zad6()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
