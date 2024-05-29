4019d5fa9ca435097cf73ba7b682a05f4b206919
Krzysztof174 <174903@student.uwm.edu.pl>
2024-05-29 15:15:49 +0200

    kwerendy mysql

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ def zad2():
     #print(dane)
     #print(dane[dane["Liczba"] > 1000])
     #print(dane[dane["Imie"] == "KRZYSZTOF"])
-    #print(dane.groupby("Rok").sum("Liczba"))
+    #print(dane.sum()["Liczba"])
     #print(dane[(dane["Rok"] <= 2005) & (dane["Rok"] >= 2000)].sum()["Liczba"])
     #print(dane[dane["Plec"] == "M"].sum()["Liczba"])
     #print(dane[dane["Plec"] == "K"].sum()["Liczba"])
+    #print(dane.groupby(["Rok", "Plec"]).head(1))
+    #print(dane.groupby(["Imie", "Plec"]).sum().sort_values("Liczba").tail(2))
+
+def zad3():
+    dane = pandas.read_csv("datasets/zamowienia.csv", sep=";", delimiter=None, decimal='.')
+    #1
+
+    #2
+    #print(dane.sort_values(by="Utarg").tail(5))
+    #3
+    #print(dane["idZamowienia"].value_counts())
 
 
 if __name__ == '__main__':
     zad2()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    zad3()
